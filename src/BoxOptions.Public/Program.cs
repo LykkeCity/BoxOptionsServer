f4442e56a88ef2ffd130bea160cc0b76baeb91a1
Dmitry Samodov <samodov.dmitry@gmail.com>
2017-04-25 18:40:26 +0300

    added threads count parameter

--- src/BoxOptions.Public/Program.cs
+++ src/BoxOptions.Public/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace BoxOptions.Public
 {
@@ -7,8 +9,35 @@ namespace BoxOptions.Public
     {
         public static void Main(string[] args)
         {
+            var cfgBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile("appsettings.dev.json", true, true)
+                .AddEnvironmentVariables();
+
+            var configuration = cfgBuilder.Build();
+
+            int kestrelThreadsCount = 1;
+            string threadsCount = configuration["KestrelThreadCount"];
+
+            if (threadsCount != null)
+            {
+                if (!int.TryParse(threadsCount, out kestrelThreadsCount))
+                {
+                    Console.WriteLine($"Can't parse KestrelThreadsCount value '{threadsCount}'");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"Kestrel threads count: {kestrelThreadsCount}");
+
             var host = new WebHostBuilder()
-                .UseKestrel(options => options.ThreadCount = 1)
+                .UseKestrel(options =>
+                {
+                    if (kestrelThreadsCount > 0)
+                    {
+                        options.ThreadCount = kestrelThreadsCount;
+                    }
+                })
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseUrls("http://*:5000")
