ffa4d41a85dfbfa2a25f080c8b7c6ecda4e6ade4
Nuno Araujo <nunobaraujo@hotmail.com>
2017-05-13 14:21:39 +0100

    Added Unit testing Project

---
+++ src/XUnitTests/Services.cs
@@ -0,0 +1,28 @@
+﻿using BoxOptions.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTests
+{
+    
+    public class Services
+    {
+        [Fact]
+        public void AssetQuoteSubscriber_construct()
+        {
+            BoxOptions.Common.BoxOptionsSettings settings = new BoxOptions.Common.BoxOptionsSettings();
+            AssetQuoteSubscriber acs = new AssetQuoteSubscriber(settings);
+            acs.MessageReceived += Acs_MessageReceived;
+            acs.Start();
+
+            acs.Dispose();
+        }
+
+        private void Acs_MessageReceived(object sender, AssetPairBid e)
+        {
+            
+        }
+    }
+}
