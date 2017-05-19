b92219af40d2ff4f1def28d5323a7e1d1ae6a550
Nuno Araujo <nunobaraujo@hotmail.com>
2017-05-19 17:30:51 +0100

    Changed CoefModels

--- src/BoxOptions.Public/Models/CoefModels.cs
+++ src/BoxOptions.Public/Models/CoefModels.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace BoxOptions.Public.Models
 {
@@ -6,15 +8,90 @@ namespace BoxOptions.Public.Models
     {
         public class CoefRequestResult
         {
-            public Coeff[][] Coeffs { get; set; }
+            public List<CoefBlock> CoefBlocks { get; set; }
+
+            public CoefRequestResult()
+            {
+                CoefBlocks = new List<CoefBlock>();
+            }
+
+            internal static CoefRequestResult Parse(string result)
+            {
+                System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("en-us");
+
+                CoefRequestResult retval = new CoefRequestResult();
+
+
+
+                List<CoefBlock> blockList = new List<CoefBlock>();
+
+                if (result.Length<2 || !result.StartsWith("[")|| !result.EndsWith("]"))
+                    throw new FormatException("Invalid format");
+                string objectstring = result.Substring(1, result.Length - 2);
+
+                string[] blocks = objectstring.Split('[');
+                foreach(var block in blocks)
+                {
+                    CoefBlock newBlock = new CoefBlock();
+                    List<Coeff> coefBlockList = new List<Coeff>();
+                    if (block.Length < 1)
+                        continue;
+                    string[] coefs = block.Split('{');
+                    Console.WriteLine(coefs);
+                    foreach (var coef in coefs)
+                    {
+                        if (coef.Length < 1)
+                            continue;
+
+                        string coefstring = "";
+                        if (coef.EndsWith("},")|| coef.EndsWith("}]"))
+                            coefstring = coef.Substring(0, coef.Length - 2); 
+                        else if (coef.EndsWith("}],"))
+                            coefstring = coef.Substring(0, coef.Length - 3);
+                        else
+                            throw new FormatException("invalid Format");
+                        
+                        string[] coefValues = coefstring.Split(',');                        
+                        Coeff newItem = new Coeff();
+                        foreach (var item in coefValues)
+                        {
+                            string[] valuePair = item.Split(':');
+
+                            if (valuePair[0] == "\"hitCoeff\"")
+                            {
+                                newItem.HitCoeff = decimal.Parse(valuePair[1], ci);
+                                Console.WriteLine("HitCoeff:\n\t{0}\n\t{1:F16}", valuePair[1], newItem.HitCoeff);
+                            }
+                            else if (valuePair[0] == "\"missCoeff\"")
+                            {
+                                newItem.MissCoeff = decimal.Parse(valuePair[1], ci);
+                                Console.WriteLine("MissCoeff :\n\t{0}\n\t{1:F16}", valuePair[1], newItem.MissCoeff);
+                            }
+                        }
+                        coefBlockList.Add(newItem);
+                    }
+                    newBlock.Coeffs = coefBlockList.ToArray();
+                    retval.CoefBlocks.Add(newBlock);
+                }
+
+                return retval;
+
+            }
+        }
+
+        public class CoefBlock
+        {
+            public Coeff[] Coeffs { get; set; }
         }
 
         public class Coeff
         {
             [JsonProperty("hitCoeff")]
-            public float HitCoeff { get; set; }
+            public decimal HitCoeff { get; set; }
             [JsonProperty("missCoeff")]
-            public float MissCoeff { get; set; }
+            public decimal MissCoeff { get; set; }
         }
+
+        
     }
 }
