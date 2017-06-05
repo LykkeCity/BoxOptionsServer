64accede5dc7bb15ce9bbb6c8a31e13f9511917b
Nuno Araujo <nunobaraujo@hotmail.com>
2017-06-05 14:36:47 +0100

    Prices published to WAMP topic are only published if both values(bid/ask) have changed.

--- src/BoxOptions.Services/Services/PriceFeedService.cs
+++ src/BoxOptions.Services/Services/PriceFeedService.cs
@@ -1,10 +1,9 @@
 ﻿using Autofac;
 using BoxOptions.Common;
 using BoxOptions.Common.Interfaces;
-using BoxOptions.Core.Interfaces;
 using BoxOptions.Core.Models;
-using Common;
 using System;
+using System.Collections.Generic;
 using System.Reactive.Subjects;
 using System.Threading.Tasks;
 using WampSharp.V2.Realm;
@@ -26,15 +25,15 @@ namespace BoxOptions.Services
         /// Asset Quote Subscriber
         /// </summary>
         IAssetQuoteSubscriber subscriber;
-       
 
+        Dictionary<string, InstrumentPrice> lastPrices;
 
 
         public PriceFeedService(BoxOptionsSettings settings, IWampHostedRealm realm, IAssetQuoteSubscriber subscriber)
         {
             this.settings = settings;            
             this.subscriber = subscriber;
-
+            lastPrices = new Dictionary<string, InstrumentPrice>();
             subject = realm.Services.GetSubject<InstrumentPrice>(this.settings.BoxOptionsApi.PricesSettingsBoxOptions.PricesTopicName);         
         }
 
@@ -80,14 +79,47 @@ namespace BoxOptions.Services
             {
                 return Task.FromResult(0);
             }
-
-            // If assetbid has Ask and Bid prices, publish to Wamp Topic
-            if (assetBid.Ask > 0 && assetBid.Bid > 0)
+            bool publish = false;            
+            if (!lastPrices.ContainsKey(assetBid.Instrument))
             {
-                // Publish object to WAMP topic
-                PublishInstrumentPair(assetBid);
+                // Asset not in history, add it and set publish = true
+                lastPrices.Add(assetBid.Instrument, assetBid);
+                publish = true;
+            }
+            else
+            {
+                if (lastPrices[assetBid.Instrument].Ask == assetBid.Ask ||
+                    lastPrices[assetBid.Instrument].Bid == assetBid.Bid)
+                {
+                    // One price (Ask or Bid) has not changed. do not publish it
+                    // Must only be published when both Ask Bid prices have changed
+                    Console.WriteLine("Not Published [{4}]: {0}/{1} -> {2}/{3}",
+                        lastPrices[assetBid.Instrument].Ask, lastPrices[assetBid.Instrument].Bid,
+                        assetBid.Ask, assetBid.Bid, 
+                        assetBid.Instrument);
+                    publish = false;
+                }
+                else
+                {
+                    // Both prices have changed publish it and assign this new bid to history
+                    publish = true;
+                    lastPrices[assetBid.Instrument] = assetBid;
+                }
             }
 
+            // If assetbid has a zero price (Ask or Bid) do NOT publish it
+            if (assetBid.Ask <= 0 || assetBid.Bid <= 0)
+            {
+                publish = false;
+                Console.WriteLine("Not Published [{0}]: Zero Value", assetBid.Instrument);
+            }
+
+            // Publish object to WAMP topic
+            if (publish)
+            {
+                Console.WriteLine("Published [{0}]:{1}", assetBid.Instrument, assetBid);
+                PublishInstrumentPair(assetBid);
+            }
             return Task.FromResult(0);
         }
 
