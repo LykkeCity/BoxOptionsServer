21331e3e6743310bb56c63832898aa60170230f6
Nuno Araujo <nunobaraujo@hotmail.com>
2017-12-16 18:59:43 +0000

    Updating Volatility Estimator

--- src/BoxOptions.CoefficientCalculator/Algo/VolatilityEstimator.cs
+++ src/BoxOptions.CoefficientCalculator/Algo/VolatilityEstimator.cs
@@ -20,34 +20,38 @@ namespace BoxOptions.CoefficientCalculator.Algo
         public double prevVolat;
         public double volat;
         public bool initialized;
+        public long timeWeekendStarts, timeWeekendEnds;
+        public bool hasWeekend;
+        public bool addedWeekend; // shows whether we've already added a weekend to the list of IE or not
 
-        public VolatilityEstimator(List<double> activityDistribution, List<Price> historicPrices, double delta, long movingWindow, double periodsPerYear)
+        public VolatilityEstimator(List<double> activityDistribution, List<Price> historicPrices, double delta, long movingWindow, double periodsPerYear, bool hasWeekend)
         {
-            runner = new Runner(delta, delta, -1);
+            runner = new Runner(delta, delta, -1, movingWindow);
             this.activityDistribution = activityDistribution;
             this.historicPrices = historicPrices;
             this.movingWindow = movingWindow;
             this.periodsPerYear = periodsPerYear;
-            //TODO: Check Java Compat: 
-            //timeOfBar = msInWeek / activityDistribution.size(); // divide number of ms per week to the number of bars
-            timeOfBar = msInWeek / activityDistribution.Count; // divide number of ms per week to the number of bars
+            this.hasWeekend = hasWeekend;
+            timeOfBar = msInWeek / activityDistribution.size(); // divide number of ms per week to the number of bars
             initialized = false;
-        }
-
-        private double ComputeAnnualVolat(List<Price> historicPrices)
-        {
-            foreach (Price aPrice in historicPrices)
+            if (hasWeekend)
             {
-                runner.Run(aPrice);
+                timeWeekendStarts = 421200000L;
+                timeWeekendEnds = 586800000L;
             }
-            double sqrtVar = runner.ComputeSqrtVar();
-            return Math.Sqrt((sqrtVar * periodsPerYear));
+            else
+            {
+                timeWeekendStarts = timeWeekendEnds = -1L;
+            }
+            addedWeekend = false;
+            DateTimeZone.setDefault(DateTimeZone.UTC); // it is an important field: without this the algorithm will
+                                                       // interpret time like my local time. https://stackoverflow.com/questions/9397715/defaulting-date-time-zone-to-utc-for-jodatimes-datetime
+                                                       
         }
-
-        private double ComputeAverageActivity(Price currentPrice, long optEndsInMs)
+        private double ComputeAverageFutureActivity(Price currentPrice, long optEndsInMs)
         { // average activity for a given period of time
 
-            long msFromMonday = MsFromMonday(currentPrice.Time);
+            long msFromMonday = MsFromMonday(currentPrice.getTime());
             int firstBar = (int)(msFromMonday / timeOfBar);
             int lastBar = (int)((msFromMonday + optEndsInMs) / timeOfBar);
             nBarStartTime = firstBar;
@@ -55,12 +59,11 @@ namespace BoxOptions.CoefficientCalculator.Algo
             double sumActivity = 0.0;
             for (int iBar = firstBar; iBar <= lastBar; iBar++)
             {
-                //sumActivity += activityDistribution.get(iBar % activityDistribution.size()); // will iterate the array if index is too big
-                sumActivity += activityDistribution[iBar % activityDistribution.Count]; // will iterate the array if index is too big
+                sumActivity += activityDistribution.get(iBar % activityDistribution.size()); // will iterate the array if index is too big
             }
-            return sumActivity / (double)(lastBar - firstBar + 1);
+            return sumActivity / (double)(lastBar - firstBar + 1); // finds average activity till the end of the box
         }
-
+       
         private long MsFromMonday(long currentTime)
         {
             // This methods was converted from JAVA, [currentTime] parameter should represent milliseconds since JAVA epoch:
@@ -102,35 +105,57 @@ namespace BoxOptions.CoefficientCalculator.Algo
         }
         */
 
+        private void Initialize(List<Price> historicalPrices)
+        {
+            Price previousPrice = historicalPrices.get(0);
+            long weekendLength = timeWeekendEnds - timeWeekendStarts;
+            foreach (var currentPrice in historicalPrices)
+            {   
+                if (currentPrice.Time - previousPrice.Time > weekendLength / 2)
+                { // to catch a weekend gap. At least half, to be sure.
+                    runner.AddTimeToIEs(timeWeekendEnds - timeWeekendStarts);
+                }
+                runner.Run(currentPrice);
+                previousPrice = currentPrice.ClonePrice();
+            }
+        }
+
+
         public double Run(List<Price> newPrices, Price currentPrice, long optEndsInMs)
         {
             if (!initialized)
             {
                 initialized = true;
-                double annualVolat = ComputeAnnualVolat(historicPrices);
-                double coeff = ComputeAverageActivity(currentPrice, optEndsInMs);
-                volat = annualVolat * coeff;
-                prevVolat = volat;
+                Initialize(historicPrices); // just initializes all runners and finds historical IEs
+                prevVolat = volat = 0.0; // this part prevents wrong payouts on weekends
                 return volat;
 
             }
             else
             {
-                foreach (Price aPrice in newPrices)
+                if (Tools.checkIfNowIsWeekend(timeWeekendStarts, timeWeekendEnds, currentPrice.Time))
                 {
-                    runner.Run(aPrice);
+                    if (!addedWeekend)
+                    {
+                        runner.AddTimeToIEs(timeWeekendEnds - timeWeekendStarts);
+                        addedWeekend = true;
+                    }
+                    prevVolat = volat;
+                    volat = 0.0;
                 }
-                while ((runner.numberDC != 0) && (runner.timesDC.First() < currentPrice.Time - movingWindow))
+                else
                 {
-                    runner.timesDC.RemoveFirst();
-                    runner.osLengths.RemoveFirst();
-                    runner.numberDC -= 1;
+                    foreach (var aPrice in newPrices)
+                    {
+                        runner.Run(aPrice);
+                    }
+                    double updatedSqrtVar = runner.ComputeTotalSqrtVar();
+                    double annualVolat = Math.Sqrt((updatedSqrtVar * periodsPerYear));
+                    double coeff = ComputeAverageFutureActivity(currentPrice, optEndsInMs);
+                    prevVolat = volat;
+                    volat = annualVolat * coeff;
+                    addedWeekend = false;
                 }
-                double updatedSqrtVar = runner.ComputeSqrtVar();
-                double annualVolat = Math.Sqrt((updatedSqrtVar * periodsPerYear));
-                double coeff = ComputeAverageActivity(currentPrice, optEndsInMs);
-                prevVolat = volat;
-                volat = annualVolat * coeff;
                 return volat;
             }
         }
